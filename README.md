9c98cfd7bba3c5cf9e965ce4d10358115ae023f6
ogulcanocakli <ogulcan.ocakli@gmail.com>
2022-11-24 10:33:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather.Utilities.TemperatureConverter
+ This package will help in converting temperature between Fahrenheit and Degree Celcius
---
+++ Weather.Utilities.TemperatureConverter/Converter.cs
@@ -0,0 +1,14 @@
+﻿namespace Weather.Utilities.TemperatureConverter
+{
+    public static class Converter
+    {
+        public static double ToFahrenheit(double degreeCelciusTemp)
+        {
+            return (degreeCelciusTemp * 9 / 5) + 32;
+        }
+        public static double ToDegreeCelcius(double fahrenheitTemp)
+        {
+            return (fahrenheitTemp - 32) * 5 / 9;
+        }
+    }
+}
